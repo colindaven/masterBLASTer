8d6f5ca0d7b55450bee75864c1f1e6d21cae479d
Colin Davenport <colindaven@hotmail.com>
2018-07-10 16:59:24 +0200

    Update merge.py
    Add best blast hit only merging in merge script only. Should be added to config.sh

--- merge.py
+++ merge.py
@@ -6,6 +6,8 @@ import numpy as np
 # Extract command line output filename
 outfile = sys.argv[1]
 
+bestBLASTHitsOnly=True
+
 def count_hits(row, hit_table):
     count = sum(protein_list == row['name'])
     return count
@@ -14,11 +16,15 @@ filelist=os.listdir("temp/")
 
 protein_list = np.array([],dtype = 'str') 
 
-# Parse blast files and combine the results into a single vector
+# Parse blast files if not empty and combine the results into a single vector
 for file in filelist:
     if file.startswith('blastout'):
-        temp_table = pd.read_table(f"temp/{file}", delimiter = '\t', header = None, usecols = [1])
-        protein_list = np.append(protein_list, temp_table.iloc[:,0].ravel())
+        if os.path.getsize(file) > 0:
+            if (bestBLASTHitsOnly):
+                temp_table = pd.read_table(f"temp/{file}", delimiter = '\t', header = None, nrows = 1, usecols = [2])
+            else:
+                temp_table = pd.read_table(f"temp/{file}", delimiter = '\t', header = None, usecols = [2])
+            protein_list = np.append(protein_list, temp_table.iloc[:,0].ravel())
 
 unique_counts = np.unique(protein_list, return_counts=True)
 results_table = pd.DataFrame({'name':unique_counts[0], 'count':unique_counts[1]})
